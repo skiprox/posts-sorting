5cce40c1f51421e181241014c88536c072b8a74e
skiprox <seanmichaelscanlan@gmail.com>
2019-03-10 16:49:12 -0400

    Adding in stuff to run from command line

--- README.md
+++ README.md
@@ -4,10 +4,15 @@
 
 ### To Run
 
-Clone this repo and create a `posts.json` file based on the `posts.example.json` file.
-
-Then just run:
+Clone this repo and create a `posts.json` file based on the `posts.example.json` file. Then run 
 
 ```
-node index.js
+npm install
 ```
+
+To get the necessary packages. Finally, just run e.g.
+
+```
+npm run --silent sort01
+```
+
--- index.js
+++ index.js
@@ -2,26 +2,39 @@
 
 const posts = require('./posts.json');
 
-class SortPosts {
-	constructor() {
-
+// HELPERS
+/**
+ * Returns two objects, sorted arrays of names
+ * of good and bad posters
+ */
+const returnSorted = (type, sortedArray) => {
+	let midpoint = Math.floor(sortedArray.length/2);
+	let goodPosters = sortedArray.slice(0, midpoint).map(item => item.name);
+	let badPosters = sortedArray.slice(midpoint, sortedArray.length).map(item => item.name);
+	let obj = {
+		"Good Posters": goodPosters,
+		"Bad Posters": badPosters
 	}
+	return obj;
+}
+
+module.exports = {
 	/**
 	 * Sorts by how long a persons post is,
 	 * bad posters are those who have shorter posts
 	 */
-	sortByLength() {
+	sortByLength: () => {
 		let week03 = posts.week03;
 		week03 = week03.sort((a, b) => {
 			return b.post.length - a.post.length;
 		});
-		this.returnSorted("Length", week03);
-	}
+		return returnSorted("Length", week03);
+	},
 	/**
 	 * Sorts by how many times a person uses "I" or "me" or "personally" in a post,
 	 * bad posters are those that use it more
 	 */
-	sortByPersonal() {
+	sortByPersonal: () => {
 		let week03 = posts.week03;
 		week03 = week03.sort((a, b) => {
 			var countA = (a.post.match(/ me /g) || []).length;
@@ -32,27 +45,7 @@ class SortPosts {
 			countB += (b.post.match(/personally/g) || []).length;
 			return countA - countB;
 		});
-		this.returnSorted("Personal", week03);
+		return returnSorted("Personal", week03);
 	}
-	/**
-	 * Returns two objects, sorted arrays of names
-	 * of good and bad posters
-	 */
-	returnSorted(type, sortedArray) {
-		console.log(`Sorted by ${type}`);
-		let midpoint = Math.floor(sortedArray.length/2);
-		let goodPosters = {
-			"good_posters": sortedArray.slice(0, midpoint).map(item => item.name)
-		};
-		let badPosters = {
-			"bad_posters": sortedArray.slice(midpoint, sortedArray.length).map(item => item.name)
-		};
-		console.log(goodPosters);
-		console.log(badPosters);
-	}
-}
-
-let sorter = new SortPosts();
-
-sorter.sortByLength();
-sorter.sortByPersonal();
+};
+const makeRunnable = require('make-runnable');
